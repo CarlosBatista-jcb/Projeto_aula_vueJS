c390a172ee42808df71e9d17e3eb1fbc9dce24e3
testegithob aula Santander coded <cbsites10@gmail.com>
2023-10-06 15:21:41 -0300

    Inicio

---
+++ src/components/Footer.vue
@@ -0,0 +1,26 @@
+<template>
+    <footer id="footer">
+    <P>Make Your Burger &copy; 2023</P>
+    </footer>
+</template>
+<Script>
+    export default {
+        name: "Footer"
+
+    }
+</Script>
+
+<style scoped>
+
+  #footer {
+    height: 200px;
+    background-color: #222;
+    border-top: 4px solid #111;
+    color: #fcba03;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    bottom: 0;
+    width: 100%;
+  }
+</style>
